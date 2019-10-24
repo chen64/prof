e74e86a0638973b93b636a5b8b1235da24cbb5e2
Jayson <jaysonchen64@gmail.com>
2019-10-24 18:35:46 -0500

    u

--- profile/src/pages/Project.js
+++ profile/src/pages/Project.js
@@ -1,61 +1,89 @@
-import React from "react"
+import React from "react";
 import { Link } from "react-router-dom";
-import "./project.css"
+import "./project.css";
 
 const Project = () => (
-    <div className="container" id="project">
-        <div className="title">Projects</div>
-        <div className="row">
-            <div className="col-md-4">
-                <div className="card">
-                    <div className="card-body">
-                        <h4 className="card-title">Picture Game</h4>
-                        <p className="card-text">Click on all pictures once to win.</p>
-                        <div className="row">
-                            <div className="col-md-4">
-                                <a href="https://github.com/chen64" className="nav-link">Github</a>
-                            </div>
-                            <div className="col-md-4">
-                                <a href="https://gentle-forest-68010.herokuapp.com/" className="nav-link">Link</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  <div className="container" id="project">
+    <div className="title">Projects</div>
+    <div className="row">
+      <div className="col-md-4">
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title">Picture Game</h4>
+            <p className="card-text">Click on all pictures once to win.</p>
+            <div className="row">
+              <div className="col-md-4">
+                <a href="https://github.com/chen64" className="nav-link">
+                  Github
+                </a>
+              </div>
+              <div className="col-md-4">
+                <a
+                  href="https://gentle-forest-68010.herokuapp.com/"
+                  className="nav-link"
+                >
+                  Link
+                </a>
+              </div>
             </div>
-            <div className="col-md-4">
-                <div className="card">
-                    <div className="card-body">
-                        <h4 className="card-title">Gems</h4>
-                        <p className="card-text">Click on each gem to earn points and match the score to win.</p>
-                        <div className="row">
-                            <div className="col-md-4">
-                                <a href="https://github.com/chen64/week-4-game" className="nav-link">Github</a>
-                            </div>
-                            <div className="col-md-4">
-                                <a href="https://young-plateau-28428.herokuapp.com" className="nav-link">Link</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="col-md-4">
-                <div className="card">
-                    <div className="card-body">
-                        <h4 className="card-title">Food search</h4>
-                        <p className="card-text">Enter a zip code to find nearby restraurants.</p>
-                        <div className="row">
-                            <div className="col-md-4">
-                                <a href="https://github.com/chen64/Weather" className="nav-link">Github</a>
-                            </div>
-                            <div className="col-md-4">
-                                <a href="https://peaceful-hollows-72695.herokuapp.com" className="nav-link">Link</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
+      </div>
+      <div className="col-md-4">
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title">Email Send</h4>
+            <p className="card-text">
+              Log in and get credits to send emails then recieve responses.
+            </p>
+            <div className="row">
+              <div className="col-md-4">
+                <a href="https://github.com/chen64/reactS" className="nav-link">
+                  Github
+                </a>
+              </div>
+              <div className="col-md-4">
+                <a
+                  href="https://calm-shelf-44720.herokuapp.com"
+                  className="nav-link"
+                >
+                  Link
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-4">
+        <div className="card">
+          <div className="card-body">
+            <h4 className="card-title">Food search</h4>
+            <p className="card-text">
+              Enter a zip code to find nearby restraurants.
+            </p>
+            <div className="row">
+              <div className="col-md-4">
+                <a
+                  href="https://github.com/chen64/Weather"
+                  className="nav-link"
+                >
+                  Github
+                </a>
+              </div>
+              <div className="col-md-4">
+                <a
+                  href="https://peaceful-hollows-72695.herokuapp.com"
+                  className="nav-link"
+                >
+                  Link
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 );
 
 export default Project;
